a9ed5d45c00aff6767aca001f31b3edc31c00979
lucaseveron <veronlucasezequiel@hotmail.es>
2025-04-11 16:22:37 -0300

    pipeline exitoso

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
 const express = require('express');
-const cron = require('node-cron');
-
 const app = express();
-let times = 0;
-
-cron.schedule('*/5 * * * * *', () => {
-  times++;
-  console.log('Tick cada 5 segundos', times);
-});
 
 app.get('/', (req, res) => {
-  res.send(`Tics ejecutados: ${times}`);
+  res.send('Hola mundo');
 });
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
+
