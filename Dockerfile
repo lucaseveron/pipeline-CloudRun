0bc65dc30d2669c3b89f4d915a4f98d9b82589a7
lucaseveron <veronlucasezequiel@hotmail.es>
2025-04-11 15:37:20 -0300

    Holaaa

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,14 @@
-# Imagen base liviana
 FROM node:19.2-alpine3.16
 
-# Crear carpeta de trabajo
 WORKDIR /app
 
-# Copiar archivos necesarios
 COPY app.js package.json ./
 
-# Instalar dependencias
 RUN npm install
 
-# Expone el puerto requerido por Cloud Run
 EXPOSE 8080
 
-# Comando para iniciar la app
 CMD ["node", "app.js"]
+
 
 
--- app.js
+++ app.js
@@ -4,19 +4,16 @@ const cron = require('node-cron');
 const app = express();
 let times = 0;
 
-cron.schedule('1-59/5 * * * * *', () => {
+cron.schedule('*/5 * * * * *', () => {
   times++;
   console.log('Tick cada 5 segundos', times);
 });
 
-// Ruta para que Cloud Run tenga algo que responder
 app.get('/', (req, res) => {
   res.send(`Tics ejecutados: ${times}`);
 });
 
-// Iniciar servidor en puerto que Cloud Run espera
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
-
